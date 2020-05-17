52d971dfd4bc591c15db8013e8003a7869ec4bba
nick <nicholasbhall91@gmail.com>
2020-05-17 19:36:23 -0400

    Init

--- mission-planning-dashboard/README.md
+++ mission-planning-dashboard/README.md
@@ -1,6 +1,6 @@
 # MissionPlanningDashboard
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.2.2.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.9.
 
 ## Development server
 
--- mission-planning-dashboard/src/app/app.module.ts
+++ mission-planning-dashboard/src/app/app.module.ts
@@ -2,18 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { CrewComponent } from './crew/crew.component';
-import { EquipmentComponent } from './equipment/equipment.component';
-import { ExperimentsComponent } from './experiments/experiments.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    CrewComponent,
-    EquipmentComponent,
-    ExperimentsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
